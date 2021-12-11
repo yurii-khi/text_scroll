f40dba20e544cf8bc91b2e8cc9b57afb2d8d91f4
yurii-khi <yuriikhiznichenko@gmail.com>
2021-12-11 18:23:30 +0100

    Add TextScroller placeholder widget

--- lib/text_scroller.dart
+++ lib/text_scroller.dart
@@ -1,7 +1,19 @@
 library text_scroller;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class TextScroller extends StatefulWidget {
+  const TextScroller({Key? key, required this.text}) : super(key: key);
+
+  final String text;
+
+  @override
+  _TextScrollerState createState() => _TextScrollerState();
+}
+
+class _TextScrollerState extends State<TextScroller> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(widget.text);
+  }
 }
